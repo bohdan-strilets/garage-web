cc77b6db3c629b3d4d4847cad948992a588ca426
BS <bohdan.strilets@gmail.com>
2025-12-11 16:41:30 +0100

    chore(app): moving app and main files to the app directory, add settings for lint staged

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div>
+      <p>Hello Vite+Rract</p>
+    </div>
+  );
+};
+
+export default App;
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const rootDiv = document.getElementById('root') as HTMLDivElement;
+
+createRoot(rootDiv).render(
   <StrictMode>
     <App />
   </StrictMode>,
