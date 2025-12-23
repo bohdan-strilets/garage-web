17ff8e24ba1c89fa8060863013071130d1bfdb4d
BS <bohdan.strilets@gmail.com>
2025-12-23 09:06:16 +0100

    refactor(mediaQueries): clarified the rules for media query

--- src/shared/styles/utils/mediaQueries.ts
+++ src/shared/styles/utils/mediaQueries.ts
@@ -1,8 +1,14 @@
 import { breakpoints } from "../tokens/breakpoints";
 
+const { desktop, laptop, tablet } = breakpoints;
+
+const mobileMax = tablet - 1;
+const tabletMax = laptop - 1;
+const laptopMax = desktop - 1;
+
 export const mediaQueries = {
-  mobile: `screen and (max-width: ${breakpoints.mobile}px)`,
-  tablet: `screen and (min-width: ${breakpoints.tablet}px)`,
-  laptop: `screen and (min-width: ${breakpoints.laptop}px)`,
-  desktop: `screen and (min-width: ${breakpoints.desktop}px)`,
+  mobile: `screen and (max-width: ${mobileMax}px)`,
+  tablet: `screen and (min-width: ${tablet}px) and (max-width: ${tabletMax}px)`,
+  laptop: `screen and (min-width: ${laptop}px) and (max-width: ${laptopMax}px)`,
+  desktop: `screen and (min-width: ${desktop}px)`,
 } as const;
