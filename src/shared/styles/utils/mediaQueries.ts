ab0ace4e05651b5239b0211d664903ea2830004b
BS <bohdan.strilets@gmail.com>
2025-12-28 16:50:00 +0100

    refactor(mediaQuery): changed semantic media queries based on breakpoint tokens

--- src/shared/styles/utils/mediaQueries.ts
+++ src/shared/styles/utils/mediaQueries.ts
@@ -3,12 +3,10 @@ import { breakpoints } from "../tokens/breakpoints";
 const { desktop, laptop, tablet } = breakpoints;
 
 const mobileMax = tablet - 1;
-const tabletMax = laptop - 1;
-const laptopMax = desktop - 1;
 
 export const mediaQueries = {
   mobile: `screen and (max-width: ${mobileMax}px)`,
-  tablet: `screen and (min-width: ${tablet}px) and (max-width: ${tabletMax}px)`,
-  laptop: `screen and (min-width: ${laptop}px) and (max-width: ${laptopMax}px)`,
+  tablet: `screen and (min-width: ${tablet}px)`,
+  laptop: `screen and (min-width: ${laptop}px)`,
   desktop: `screen and (min-width: ${desktop}px)`,
 } as const;
