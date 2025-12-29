b3f1c449801a808e850b1f3d7bffcb61876bc3b7
BS <bohdan.strilets@gmail.com>
2025-12-29 20:42:21 +0100

    refactor: added additional folders to pages for better separation

--- src/routes/_app/index.tsx
+++ src/routes/_app/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { HomePage } from "@pages/HomePage";
+import { HomePage } from "@pages/app/HomePage";
 
 export const Route = createFileRoute("/_app/")({
   component: () => <HomePage />,
--- src/routes/_auth/register.tsx
+++ src/routes/_auth/register.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { RegisterPage } from "@pages/RegisterPage";
+import { RegisterPage } from "@pages/auth/RegisterPage";
 
 export const Route = createFileRoute("/_auth/register")({
   component: () => <RegisterPage />,
