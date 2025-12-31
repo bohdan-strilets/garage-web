35a4552c206e9c585ef746d32b56c6eff08c608f
BS <bohdan.strilets@gmail.com>
2025-12-31 09:54:26 +0100

    refactor(ui): added additional gap

--- src/shared/ui/controls/ButtonGroup/ButtonGroup.css.ts
+++ src/shared/ui/controls/ButtonGroup/ButtonGroup.css.ts
@@ -6,7 +6,6 @@ export const root = recipe({
   base: {
     display: "inline-flex",
     alignItems: "stretch",
-    gap: themeContract.space.sm,
   },
 
   variants: {
@@ -14,9 +13,17 @@ export const root = recipe({
       horizontal: { flexDirection: "row" },
       vertical: { flexDirection: "column" },
     },
+
+    gap: {
+      sm: { gap: themeContract.space.sm },
+      md: { gap: themeContract.space.md },
+      lg: { gap: themeContract.space.lg },
+      xl: { gap: themeContract.space.xl },
+    },
   },
 
   defaultVariants: {
     orientation: "horizontal",
+    gap: "sm",
   },
 });
