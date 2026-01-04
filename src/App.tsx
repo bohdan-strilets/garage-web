4fd2a9c5f9cc60a44b0b32efac0a3ad9dedce905
BS <bohdan.strilets@gmail.com>
2026-01-04 12:53:59 +0100

    chore: initial project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <p>Hello react APP + Vite</p>
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+const root = document.getElementById('root') as HTMLDivElement
+
+createRoot(root).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+)
