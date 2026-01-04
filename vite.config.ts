a77cfa1767993a9eb367bb4c1a6d5ff74703600f
BS <bohdan.strilets@gmail.com>
2026-01-04 17:19:18 +0100

    chore: install vanilla-extract styling dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'node:path'
 
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src/app'),
