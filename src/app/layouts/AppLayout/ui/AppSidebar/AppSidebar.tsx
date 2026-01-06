7f93964c338859cc08279bdf5f953b962249a25f
BS <bohdan.strilets@gmail.com>
2026-01-06 11:43:59 +0100

    fix(sidebar): cleaning in sidebar

--- src/app/layouts/AppLayout/ui/AppSidebar/AppSidebar.tsx
+++ src/app/layouts/AppLayout/ui/AppSidebar/AppSidebar.tsx
@@ -1,6 +1,10 @@
 import { primaryNav } from '@features/navigation'
 import Navigation from '@features/navigation/ui/Navigation'
 import Logo from '@shared/ui/branding/Logo'
+import Divider from '@shared/ui/layouts/Divider'
+import Spacer from '@shared/ui/layouts/Spacer'
+import Stack from '@shared/ui/layouts/Stack'
+import Heading from '@shared/ui/typography/Heading'
 
 import { useSidebar } from '../../model/useSidebar'
 import SidebarToggle from '../SidebarToggle'
@@ -9,12 +13,16 @@ const AppSidebar = () => {
   const { toggle, isOpen } = useSidebar()
 
   return (
-    <>
-      <Logo />
-      Bar
+    <Stack align="start" gap="md">
+      <Stack direction="row" align="center" gap="sm">
+        <Logo />
+        {isOpen && <Heading level={1}>Garage</Heading>}
+      </Stack>
+      <Divider />
       <SidebarToggle toggle={toggle} isOpen={isOpen} />
+      <Spacer size="lg" />
       <Navigation items={primaryNav} />
-    </>
+    </Stack>
   )
 }
 
