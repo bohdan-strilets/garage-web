3aa76951868b7e73da4cb8e51255f34b9e5a53e5
BS <bohdan.strilets@gmail.com>
2026-01-07 12:19:37 +0100

    fix(storageKey): moved file to separate dir

--- src/shared/styles/model/useThemeStore.ts
+++ src/shared/styles/model/useThemeStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-import { STORAGE_KEYS } from '@shared/config/storageKeys'
+import { STORAGE_KEYS } from '@shared/config/storage'
 
 import { THEME } from '../enums/Theme'
 
