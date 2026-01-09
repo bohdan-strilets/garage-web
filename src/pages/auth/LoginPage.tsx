9cf25b75274e7a0ba4cddfae1c8e088a28dcc8af
BS <bohdan.strilets@gmail.com>
2026-01-09 07:27:28 +0100

    feat(auth): added rotate link for login and register

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,12 +1,25 @@
 import { LoginForm } from '@features/auth/login'
+import { paths } from '@shared/router'
+import { TextLink } from '@shared/ui/controls/TextLink'
+import { Divider } from '@shared/ui/layouts/Divider'
 import { Stack } from '@shared/ui/layouts/Stack'
 import { Heading } from '@shared/ui/typography/Heading'
+import { Paragraph } from '@shared/ui/typography/Paragraph'
 
 const LoginPage = () => {
   return (
-    <Stack gap="md">
+    <Stack gap="lg">
       <Heading level={1}>Login</Heading>
       <LoginForm />
+
+      <Divider />
+
+      <Paragraph align="center">
+        Don’t have an account yet?{' '}
+        <TextLink to={paths.AUTH.REGISTER} underline={true}>
+          Sign up
+        </TextLink>
+      </Paragraph>
     </Stack>
   )
 }
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,12 +1,25 @@
 import { RegisterForm } from '@features/auth/register'
+import { paths } from '@shared/router'
+import { TextLink } from '@shared/ui/controls/TextLink'
+import { Divider } from '@shared/ui/layouts/Divider'
 import { Stack } from '@shared/ui/layouts/Stack'
 import { Heading } from '@shared/ui/typography/Heading'
+import { Paragraph } from '@shared/ui/typography/Paragraph'
 
 const RegisterPage = () => {
   return (
-    <Stack gap="md">
+    <Stack gap="lg">
       <Heading level={1}>Register</Heading>
       <RegisterForm />
+
+      <Divider />
+
+      <Paragraph align="center">
+        Already have an account?{' '}
+        <TextLink to={paths.AUTH.LOGIN} underline={true}>
+          Log in
+        </TextLink>
+      </Paragraph>
     </Stack>
   )
 }
